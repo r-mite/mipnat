a34aa1887bf79721a774c83f140331babdac4a7e
あるマイト <universal5b@hotmail.co.jp>
2016-02-17 03:49:51 +0900

    add cli and time
    natとの通信とアドレス書き換えを図ります

--- src/mn.c
+++ src/mn.c
@@ -43,6 +43,39 @@
 
 #include <linux/fib_rules.h>
 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h> /* for strncpy */
+#include <unistd.h> /* for close */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+
+#include <ifaddrs.h>
+#include <sys/uio.h>
+#include <sys/param.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <netdb.h>
+
+#include <sys/time.h>
+
+#define IF_NUM "eth3"
+#define PORT "5000"
+#define BUFLEN 256
+#define BACKLOG 5
+
+
+
+
 #include "debug.h"
 #include "mipv6.h"
 #include "icmp6.h"
@@ -783,11 +816,166 @@ static int mn_bu_ratelimit(struct bulentry *e)
 }
 
 
-static void mn_send_nat_if(struct home_addr_info *hai){
-	MDBG("new method\n");
-	//char ip6[] = "2002::a00:27ff:fea9:d6a1";
-	//client(ip6);
+
+
+
+char *ip6_ntoa(struct in6_addr ip6){
+	static char str [INET6_ADDRSTRLEN];
+	inet_ntop(AF_INET6, &ip6, str, INET6_ADDRSTRLEN);
+	return str;
 }
+
+struct in6_addr ip6_aton(char *str) {
+	struct in6_addr ip6;
+	inet_pton(AF_INET6, str, &ip6);
+	return ip6;
+}
+
+void getifipv6addr(struct in6_addr *ip6, const char *device){
+	struct ifaddrs *if_list = NULL;
+	struct ifaddrs *ifa = NULL;
+	void *tmp = NULL;
+
+	getifaddrs(&if_list);
+	for(ifa = if_list; ifa != NULL; ifa = ifa->ifa_next){
+		if(strcmp(ifa->ifa_name, device) == 0){
+			if(!ifa->ifa_addr){
+				continue;
+			}else{
+				if(ifa->ifa_addr->sa_family == AF_INET6){
+					*ip6 = ((struct sockaddr_in6 *)ifa->ifa_addr)->sin6_addr;
+					break;
+				}
+			}
+		}
+	}
+	freeifaddrs(if_list);
+}
+
+int tcp_connect(const char *host, const char *service){
+	int err;
+	struct addrinfo hints;
+	struct addrinfo *res = NULL;
+	struct addrinfo *ai;
+	int sock;
+	
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_NUMERICSERV;
+	
+	err = getaddrinfo(host, service, &hints, &res);
+	if(err != 0){
+		printf("getaddrinfo(): %s\n", gai_strerror(err));
+		return -1;
+	}
+	
+	ai = res;
+	sock = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+	if(sock < 0){
+		return -1;
+	}
+	int cs;
+	cs = connect(sock, ai->ai_addr, ai->ai_addrlen);
+	if(cs < 0){
+		perror("connect");
+		exit(1);
+	}
+	printf("connect.\n");
+	freeaddrinfo(res);
+	return sock;
+}
+
+int read_line(int sock, char *p){
+	int len = 0;
+	while(1){
+		int ret;
+		ret = read(sock, p, 1);
+		if(ret == -1){
+			perror("read");
+			exit(1);
+		}else if(ret == 0){
+			break;
+		}
+		if(*p == '\n'){
+			p++;
+			len++;
+			break;
+		}
+		p++;
+		len++;
+	}
+	*p = '\0';
+	return len;
+}
+
+char *client(const char *host){
+	int sock;
+	sock = tcp_connect(host, PORT);
+	if(sock < 0){
+		perror("client");
+		exit(1);
+	}
+	
+	char conbuf[] = "pull\n";
+	write(sock, conbuf, strlen(conbuf));
+	int read_size;
+	char buf[BUFLEN];
+	read_size = read_line(sock, buf);
+	if(read_size != 0)
+		printf("mes: %s\n", buf);
+	
+	printf("disconnect.\n");
+	close(sock);
+	return buf;
+}
+
+void timeclient(const char *host, char *count){
+	int sock;
+	sock = tcp_connect(host, PORT);
+	if(sock < 0){
+		perror("client");
+		exit(1);
+	}
+	
+	//char conbuf[] = "pull\n";
+	write(sock, count, strlen(conbuf));
+	int read_size;
+	char buf[BUFLEN];
+	read_size = read_line(sock, buf);
+	if(read_size != 0)
+		printf("mes: %s\n", buf);
+	
+	printf("disconnect.\n");
+	close(sock);
+	return buf;
+}
+
+double get_dtime(void){
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  return ((double)(tv.tv_sec) + (double)(tv.tv_usec) * 0.001 * 0.001);
+}
+
+static void mn_send_nat_if(struct home_addr_info *hai){
+	//time count
+	double d0, d1;
+	d0 = get_dtime();
+	struct in6_addr coaddr;
+	char ip6[] = "2002::a00:27ff:fea9:d6a1";
+	int i;
+	coaddr = ip6_aton(client(ip6));
+	for(i=0; i<16; i++){
+		hai->primary_coa.addr.s6_addr[i] = coaddr.s6_addr[i];
+	}
+	d1 = get_dtime();
+	char count[256];
+	sprintf(count, "%f\n", d1 - d0);
+	timeclient(ip6, count);
+}
+
+
+
 
 
 static void mn_send_home_bu(struct home_addr_info *hai)
